1c043d103b4c2da1512c194750e83e2cb833edcb
超 杜 <744008606@qq.com>
2020-09-08 19:34:30 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 作业1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //这是我的第一个作业12
+
+            Console.WriteLine("梦想还是要有的,万一实现了呢!");
+            Console.ReadKey();
+            Console.WriteLine("  ☆☆☆       ☆☆☆        ☆☆☆ ");
+            Console.WriteLine("☆      ☆    ☆      ☆    ☆      ☆");
+            Console.WriteLine("☆        ☆☆          ☆☆        ☆");
+            Console.WriteLine(" ☆   情    人    节   快    乐！  ☆ ");
+            Console.WriteLine("   ☆              ☆             ☆  ");
+            Console.WriteLine("     ☆          ☆  ☆          ☆    ");
+            Console.WriteLine("       ☆       ☆     ☆       ☆    ");
+            Console.WriteLine("         ☆    ☆        ☆    ☆  ");
+            Console.WriteLine("            ☆              ☆   ");
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("丨      欢迎使用xxx充值业务，请输入充值金额：   丨");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("------------------------------------------------");
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("丨          用户充值金额为100元                 丨");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("------------------------------------------------");
+            Console.ReadKey();
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("丨        充值成功，您本次充值为100元           丨");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("丨                                              丨");
+            Console.WriteLine("------------------------------------------------");
+            Console.ReadKey();
+
+
+        }
+    }
+}
